c512211f98544b98c2bc388ae34f1ecfba8a9198
Dayanybentorondon119 <dayanybentorondon119@gmail.com>
2026-08-13 21:00:56 -0300

    Update sequences.js

--- src/config/sequences.js
+++ src/config/sequences.js
@@ -40,6 +40,60 @@ Vou te enviar as opções, ok?
 *Mas atenção:* Estou liberando essa condição somente para 50 primeiras pessoas que garantirem hoje. *E para garantir que você tenha essa condição especial e acesso a todo material poderia salvar meu contato por gentileza*.`,
     delayAfter: 3,
   },
+  {
+    type: "image",
+    url: "https://i.imgur.com/C0CxYXh.jpeg",
+    caption: `*OPÇÃO 1 - GUIA DE RECEITAS GOURMET DE TORTAS GELADAS*
+
+GUIA COM 20 RECEITAS EXCLUSIVAS DE TORTAS GELADAS GOURMET.
+
+Tortinha de Limão
+Tortinha de Maracujá com Chocolate
+Tortinha de Banoffe
+Tortinha de Chocolate com Cereja
+Tortinha Kinder Bueno
+
+*E MUITO MAIS…*
+
+*TUDO POR APENAS R$ 10*`,
+    delayAfter: 3,
+  },
+  {
+    type: "image",
+    url: "https://i.imgur.com/AtkLx66.jpeg",
+    caption: `*OPÇÃO 2 - GUIA DE RECEITAS GOURMET DE TORTAS GELADAS*
+➕ *GUIA COM RECEITAS DE PUDIM GOURMET SEM FOGO*
+
+*COM 20 RECEITAS EXCLUSIVAS:*
+
+Pudim de Maracujá Sem Forno
+Pudim de Frutas Tropicais Sem Forno
+Pudim de Chocolate Cremoso Sem Forno
+Pudim de Coco com Leite Condensado Sem Forno
+Pudim de Nutella Sem Forno
+*E MUITO MAIS…*
+
+*TUDO POR APENAS R$ 17*`,
+    delayAfter: 3,
+  },
+  {
+    type: "text",
+    text: `*Qual das 2 opções você prefere amada?*
+
+*OPÇÃO 1 - R$10*
+Inclui apenas 20 receitas de torta gelada gourmet
+
+*OPÇÃO 2 - R$17 COMPLETA*
+Inclui com todas as receitas de torta gelada gourmet + as receitas de pudim sem forno gourmet com todo conteúdo que te informei acima`,
+    delayAfter: 3,
+  },
+  {
+    type: "text",
+    text: `Eu vou te enviar o material antecipadamente pois trabalhamos na honestidade *mas antes de responder, verifique se realmente possa pagar, acredito que você seja uma pessoa de Deus e honesta e que vai me pagar certinho. Se não puder me pagar nem responda pois vai prejudicar o meu trabalho❤️*
+
+Só me responder se prefere a opção 1 ou a 2 e já te envio.`,
+    delayAfter: 0,
+  },
 ];
 
 // ============ SEQUÊNCIA 2: CLIENTE QUIS SABER MAIS (não decidiu ainda) ============
